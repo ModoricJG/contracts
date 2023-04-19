cb3d007b32695b45b9c1763bfca8c70a30e7dc94
JeonGuKang <jgkang@modoric.com>
2023-04-19 11:48:35 +0900

    feat : 구매 기능 추가

--- SaleAnimalToken.sol
+++ SaleAnimalToken.sol
@@ -30,5 +30,32 @@ contract SaleAnimalToken {
         onSaleAnimalTokenArray.push(_animalTokenId);
     }
 
+    function purchaseAnimalToken(uint256 _animalTokenId) public payable {
+        uint256 price = animalTokenPrices[_animalTokenId];
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+
+
+        require(price > 0, "animal token not sale.");
+        require(price <= msg.value, "Caller sent lower than price.");
+        require(animalTokenOwner != msg.sender, "Caller is Animal Token Owner.");
+
+        payable (animalTokenOwner).transfer(msg.value);
+        //인자 1.보내는 사람 2. 받는사람 3. 뭘 보낼것인가
+        mintAnimalTokenAddress.safeTransferFrom(animalTokenOwner, msg.sender, _animalTokenId);
+
+        //초기화 
+        animalTokenPrices[_animalTokenId] = 0;
+        
+        for (uint256 i = 0; i < onSaleAnimalTokenArray.length; i++) {
+            if(animalTokenPrices[onSaleAnimalTokenArray[i]] == 0) {
+                onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[onSaleAnimalTokenArray.length -1];
+                onSaleAnimalTokenArray.pop();
+            }
+        } 
+    }
+
+    function getOnSaleAnimalTokenArrayLength() view public returns (uint256) {
+        return onSaleAnimalTokenArray.length;
+    }
 
 }
